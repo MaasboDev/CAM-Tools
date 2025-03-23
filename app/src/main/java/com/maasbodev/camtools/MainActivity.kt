2bc88876eb53881f2d032252d529899567807b83
Marçal <marcal.asbo@gmail.com>
2025-03-23 11:09:53 +0100

    mvp

--- app/src/main/java/com/maasbodev/camtools/MainActivity.kt
+++ app/src/main/java/com/maasbodev/camtools/MainActivity.kt
@@ -4,24 +4,58 @@ import android.Manifest
 import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture.OnImageCapturedCallback
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cameraswitch
+import androidx.compose.material.icons.filled.Photo
+import androidx.compose.material.icons.filled.PhotoCamera
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.maasbodev.camtools.ui.theme.CAMToolsTheme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
 	private val permissionsToRequest = arrayOf(
 		Manifest.permission.CAMERA,
 		Manifest.permission.RECORD_AUDIO,
 	)
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
@@ -47,7 +81,7 @@ class MainActivity : ComponentActivity() {
 					.reversed()
 					.forEach { permission ->
 						PermissionDialog(
-							permissionTextProvider = when(permission) {
+							permissionTextProvider = when (permission) {
 								Manifest.permission.CAMERA -> CameraPermissionTextProvider()
 								Manifest.permission.RECORD_AUDIO -> RecordAudioPermissionTextProvider()
 								else -> return@forEach
@@ -65,8 +99,113 @@ class MainActivity : ComponentActivity() {
 							onGoToAppSettingsClick = ::openAppSettings,
 						)
 					}
+				if (hasRequiredPermissions()) {
+					val scope = rememberCoroutineScope()
+					val scaffoldState = rememberBottomSheetScaffoldState()
+					val controller = remember {
+						LifecycleCameraController(applicationContext).apply {
+							setEnabledUseCases(
+								CameraController.IMAGE_CAPTURE or
+										CameraController.VIDEO_CAPTURE
+							)
+						}
+					}
+					val bitmaps by viewModel.bitmaps.collectAsState()
+					BottomSheetScaffold(
+						scaffoldState = scaffoldState,
+						sheetPeekHeight = 0.dp,
+						sheetContent = {
+							PhotoBottomSheetContent(
+								bitmaps = bitmaps,
+								modifier = Modifier
+									.fillMaxWidth(),
+							)
+						}
+					) { innerPadding ->
+						Box(
+							modifier = Modifier
+								.fillMaxSize()
+								.padding(innerPadding)
+						) {
+							CameraPreview(
+								controller = controller,
+								modifier = Modifier
+									.fillMaxSize(),
+							)
+							IconButton(
+								onClick = {
+									controller.cameraSelector =
+										if (controller.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA)
+											CameraSelector.DEFAULT_FRONT_CAMERA
+										else
+											CameraSelector.DEFAULT_BACK_CAMERA
+								},
+								modifier = Modifier
+									.offset(16.dp, 16.dp)
+							) {
+								Icon(
+									imageVector = Icons.Default.Cameraswitch,
+									contentDescription = "Switch camera", //TODO Localize
+								)
+							}
+							Row(
+								modifier = Modifier
+									.fillMaxWidth()
+									.align(Alignment.BottomCenter)
+									.padding(16.dp),
+								horizontalArrangement = Arrangement.SpaceAround,
+							) {
+								IconButton(
+									onClick = {
+										scope.launch {
+											scaffoldState.bottomSheetState.expand()
+										}
+									}
+								) {
+									Icon(
+										imageVector = Icons.Default.Photo,
+										contentDescription = "Open gallery", //TODO Localize
+									)
+								}
+								IconButton(
+									onClick = {
+										takePhoto(
+											controller = controller,
+											onPhotoTaken = viewModel::onTakePhoto
+										)
+									}
+								) {
+									Icon(
+										imageVector = Icons.Default.PhotoCamera,
+										contentDescription = "Take photo", //TODO Localize
+									)
+								}
+							}
+						}
+					}
+				}
 			}
 		}
+	}
+
+	private fun takePhoto(
+		controller: LifecycleCameraController,
+		onPhotoTaken: (Bitmap) -> Unit,
+	) {
+		controller.takePicture(
+			ContextCompat.getMainExecutor(applicationContext),
+			object : OnImageCapturedCallback() {
+				override fun onCaptureSuccess(image: ImageProxy) {
+					super.onCaptureSuccess(image)
+					onPhotoTaken(image.toBitmap())
+				}
+
+				override fun onError(exception: ImageCaptureException) {
+					super.onError(exception)
+					Log.e("Camera", "Couldn't take photo", exception)
+				}
+			}
+		)
 	}
 
 	private fun hasRequiredPermissions(): Boolean {
--- app/src/main/java/com/maasbodev/camtools/MainViewModel.kt
+++ app/src/main/java/com/maasbodev/camtools/MainViewModel.kt
@@ -1,10 +1,18 @@
 package com.maasbodev.camtools
 
+import android.graphics.Bitmap
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 	val visiblePermissionDialogQueue = mutableStateListOf<String>()
+	private val _bitmaps = MutableStateFlow<List<Bitmap>>(emptyList())
+	val bitmaps = _bitmaps.asStateFlow()
+	fun onTakePhoto(bitmap: Bitmap) {
+		_bitmaps.value += bitmap
+	}
 	fun dismissDialog() {
 		visiblePermissionDialogQueue.removeAt(visiblePermissionDialogQueue.lastIndex)
 	}
